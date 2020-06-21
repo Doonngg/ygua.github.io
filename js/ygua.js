79f9711ba35aa59bfe950a12ce21339f6427daca
Doonngg <61999334+Doonngg@users.noreply.github.com>
2020-06-21 15:08:08 +0800

    请问

--- js/ygua.js
+++ js/ygua.js
@@ -1,7 +1,27 @@
+
 window.addEventListener("load",function(){
 	
-	
+
 	
 	
 	
 })
+	var userAgent = navigator.userAgent.toLowerCase();
+	    var platform;
+	    if(userAgent == null || userAgent == ''){
+	        platform = 'WEB' ; 
+	    }else{
+	        if(userAgent.indexOf("android") != -1 ){ 
+	            platform = 'ANDROID';
+	            location.href = "mobile.html";
+	        }else if(userAgent.indexOf("ios") != -1 || userAgent.indexOf("iphone") != -1 || userAgent.indexOf("ipad") != -1){ 
+	            platform = 'IOS';
+	            location.href = "mobile.html";
+	        }else if(userAgent.indexOf("windows phone") != -1 ){ 
+	            platform = 'WP';
+	            location.href = "mobile.html";
+	        }else{
+	            platform = 'WEB' ;
+	            location.href = "index.html";
+	        };
+	    };
